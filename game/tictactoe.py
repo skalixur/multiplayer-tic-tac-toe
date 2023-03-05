e7e4cd63937140a9073a1c35da0c9af9d5efc7d7
ricsirogi <rikike888@gmail.com>
2023-03-05 02:05:58 +0100

    game

--- game/tictactoe.py
+++ game/tictactoe.py
@@ -1,4 +1,4 @@
-# I have to rewrite it :C
+# applepie.loca.lt/
 
 # importing libraries
 import tkinter as tk
@@ -7,36 +7,81 @@ import json
 # creating tkinter window
 win = tk.Tk()
 
-post_button = tk.Button(win, text="Post", command=lambda:post())
-get_button = tk.Button(win, text="Get", command=lambda:get())
-post_entry = tk.Entry(win)
-get_entry = tk.Entry(win)
+main_menu_frame = tk.Frame(win)
+game_frame = tk.Frame(win)
 
-post_button.grid(row=0, column=0)
+url_label = tk.Label(main_menu_frame, text="Type the url of your partner")
+url_entry = tk.Entry(main_menu_frame)
+url_send_button = tk.Button(main_menu_frame, text="Post", command=lambda:start())
+
+main_menu_frame.grid(row=0, column=0)
+url_label.grid(row=0, column=0)
+url_entry.grid(row=1, column=0)
+url_send_button.grid(row=2, column=0)
+
+symbol = "O"
+
+"""post_button.grid(row=0, column=0)
 get_button.grid(row=1, column=0)
 post_entry.grid(row=0, column=1)
-get_entry.grid(row=1, column=1)
-url = "https://applepie.loca.lt/"
+get_entry.grid(row=1, column=1)"""
+
+url = ""
+board=[[],[],[]]
+board_state = "_________"
+
+for i in range(3):
+    for j in range(3):
+        button = tk.Button(game_frame, text="[]", borderwidth=1, font=("Times New Roman", 14), width=14, height=7, command=lambda i=i, j=j:click(i, j, symbol))
+        board[i].append(button)
+        button.grid(row=i, column=j)
+
+get_button = tk.Button(game_frame, text="GET", command=lambda:get())
+
+def start():
+    global url
+    url = url_entry.get()
+
+    # Seeing if the user had done fucked up
+    try:
+        url_validity_check = requests.get(url)
+    except requests.exceptions.MissingSchema:
+        return print(f"Invalid URL: {url}")
+    url_validity_check = requests.get(url + "/isfirstplayer")
+    if not url_validity_check:
+        return print("Not the actual website...")
+    
+    if url[len(url)]== "/":
+        url = url.rstrip("/")
+
+    # If the user is smart
+    main_menu_frame.grid_forget()
+    game_frame.grid(row=0, column=0)
+    
+def click(row, column, symbol):
+    board[row][column]["text"] = f"[{symbol}]"
+    board_state[row*3+column] = symbol
+
+    temp = requests.post(url + f"/boardstate?boardstate={board_state}")
+    print(temp.text)
 
 def post():
     data = post_entry.get()
-    p1 = data[0:data.find(",")]
-    p2 = data[data.find(",")+1:len(data)]
-    endpoint = "players/players"
-    print(url)
-    print(url + endpoint + f"?player1={p1}&player2={p2}")
-    temp = requests.post(url + endpoint + f"?player1={p1}&player2={p2}")
+
+    endpoint = ""
+
+    """print(url)
+    print(url + endpoint + f"?player1={p1}&player2={p2}")"""
+
+    temp = requests.post(url + f"/?player1={p1}&player2={p2}"+ board_state)
     print(temp.text)
 
 def get():
-    endpoint = "players"
 
-    data = requests.get(url + endpoint + "/")
+    data = requests.get(url + "/boardstate")
     data = json.loads(json.loads(data.text))
 
-    get_data = get_entry.get()
-
-    print(data.get(get_data))
+    print(data.text)
 
 win.mainloop()
 
