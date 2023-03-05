c745033519b8bb5e0011716833a78423b5593ab3
Skqlixur <jmjmbo5@gmail.com>
2023-03-06 00:36:54 +0800

    add regex pain WHAT IS THAT (well i made it so i know)

--- server/index.js
+++ server/index.js
@@ -5,11 +5,16 @@ const port = process.env.PORT || 2199
 let boardState = 'bs:_________',
   isFirstPlayer = true,
   goesFirst = Math.random() >= 0.5,
-  turnCount = 0
+  turnCount = 0,
+  winner = null
+
+let XWinsRegex =
+  /bs:(XXX[XO_]{6})|([XO_]{3}XXX[XO_]{3})|([XO_]{6}XXX)|(X[XO_]{2}X[XO_]{2}X[XO_]{2})|([XO_]X[XO_]{2}X[XO_]{2}X[XO_])|([XO_]{2}X[XO_]{2}X[XO_]{2}X)|(X[XO_]{3}X[XO_]{3}X)|([XO_]{2}X[XO_]X[XO_]X[XO_]{2})/g
+let OWinsRegex =
+  /bs:(OOO[XO_]{6})|([XO_]{3}OOO[XO_]{3})|([XO_]{6}OOO)|(O[XO_]{2}O[XO_]{2}O[XO_]{2})|([XO_]O[XO_]{2}O[XO_]{2}O[XO_])|([XO_]{2}O[XO_]{2}O[XO_]{2}O)|(O[XO_]{3}O[XO_]{3}O)|([XO_]{2}O[XO_]O[XO_]O[XO_]{2})/g
 
 app.get('/', (req, res, next) => {
   res.status(200).send('the server is still running')
-  console.log('/ -> the server is still running')
 })
 
 app.post('/clear', (req, res, next) => {
@@ -18,29 +23,19 @@ app.post('/clear', (req, res, next) => {
   isFirstPlayer = true
   goesFirst = Math.random() >= 0.5
   res.status(200).json({ statusCode: 200, message: 'Everything cleared!' })
-  console.log('/clear -> Everything was cleared')
 })
 
 app.get('/isfirstplayer', (req, res, next) => {
-  if (isFirstPlayer) {
-    console.log({ statusCode: 200, goesFirst })
-    res.status(200).json({ statusCode: 200, goesFirst })
-  }
-  if (!isFirstPlayer) {
-    console.log({ statusCode: 200, goesFirst: !goesFirst })
+  if (isFirstPlayer) res.status(200).json({ statusCode: 200, goesFirst })
+
+  if (!isFirstPlayer)
     res.status(200).json({ statusCode: 200, goesFirst: !goesFirst })
-  }
-  if (isFirstPlayer) {
-    console.log('isFirstPlayer = false')
-    isFirstPlayer = false
-  }
+
+  if (isFirstPlayer) isFirstPlayer = false
 })
 
 app.get('/boardstate', (req, res, next) => {
   res.status(200).json({ statusCode: 200, boardState, turnCount })
-  console.log(
-    'Retruned boardstate: ' + { statusCode: 200, boardState, turnCount }
-  )
 })
 
 app.post('/boardstate', (req, res, next) => {
@@ -50,13 +45,19 @@ app.post('/boardstate', (req, res, next) => {
       .json({ statusCode: 400, reason: 'Invalid board state' })
   boardState = req.query.boardstate
   turnCount += 1 /////////////////
+
+  // Determine if there's a winner, and which symbol
+
+  if (XWinsRegex.test(boardState)) winner = 'X'
+  else if (OWinsRegex.test(boardState)) winner = 'O'
+
   res.status(200).json({
     statusCode: 200,
     boardState,
     turnCount,
+    winner,
     message: 'Board state set',
   })
-  console.log('Boardstate was posted: ' + boardState)
 })
 
 app.listen(port, () => console.log(`Listening on *:${port}`))
