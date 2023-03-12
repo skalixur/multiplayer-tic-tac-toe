51bd3f176f74c0cd37b245e8ab0d7f9675c464ea
Skqlixur <jmjmbo5@gmail.com>
2023-03-12 13:06:26 +0800

    brvuh

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
-const express = require('express')
-const app = express()
+const { Server } = require('socket.io')
 const port = process.env.PORT || 2199
-
 let boardState = 'bs:_________',
   isFirstPlayer = true,
   goesFirst = Math.random() >= 0.5,
@@ -9,57 +7,63 @@ let boardState = 'bs:_________',
   winner = null
 
 let XWinsRegex =
-  /bs:(XXX[XO_]{6})|([XO_]{3}XXX[XO_]{3})|([XO_]{6}XXX)|(X[XO_]{2}X[XO_]{2}X[XO_]{2})|([XO_]X[XO_]{2}X[XO_]{2}X[XO_])|([XO_]{2}X[XO_]{2}X[XO_]{2}X)|(X[XO_]{3}X[XO_]{3}X)|([XO_]{2}X[XO_]X[XO_]X[XO_]{2})/g
+  /(XXX[XO_]{6})|([XO_]{3}XXX[XO_]{3})|([XO_]{6}XXX)|(X[XO_]{2}X[XO_]{2}X[XO_]{2})|([XO_]X[XO_]{2}X[XO_]{2}X[XO_])|([XO_]{2}X[XO_]{2}X[XO_]{2}X)|(X[XO_]{3}X[XO_]{3}X)|([XO_]{2}X[XO_]X[XO_]X[XO_]{2})/g
 let OWinsRegex =
-  /bs:(OOO[XO_]{6})|([XO_]{3}OOO[XO_]{3})|([XO_]{6}OOO)|(O[XO_]{2}O[XO_]{2}O[XO_]{2})|([XO_]O[XO_]{2}O[XO_]{2}O[XO_])|([XO_]{2}O[XO_]{2}O[XO_]{2}O)|(O[XO_]{3}O[XO_]{3}O)|([XO_]{2}O[XO_]O[XO_]O[XO_]{2})/g
+  /(OOO[XO_]{6})|([XO_]{3}OOO[XO_]{3})|([XO_]{6}OOO)|(O[XO_]{2}O[XO_]{2}O[XO_]{2})|([XO_]O[XO_]{2}O[XO_]{2}O[XO_])|([XO_]{2}O[XO_]{2}O[XO_]{2}O)|(O[XO_]{3}O[XO_]{3}O)|([XO_]{2}O[XO_]O[XO_]O[XO_]{2})/g
 
-app.get('/', (req, res, next) => {
-  res.status(200).send('the server is still running')
-})
+const io = new Server()
 
-app.post('/clear', (req, res, next) => {
-  boardState = 'bs:_________'
-  turnCount = 0
-  isFirstPlayer = true
-  goesFirst = Math.random() >= 0.5
-  winner = null
-  res.status(200).json({ statusCode: 200, message: 'Everything cleared!' })
-})
+// io.emit('event', data) -> send to ALL clients
+// socket.broadcast.emit('event', data) -> send to ALL clients except sender
+// io.engine.clientsCount -> Number of clients
+// socket.once('one-time-event') -> Receive an event, but once
 
-app.get('/isfirstplayer', (req, res, next) => {
-  if (isFirstPlayer) res.status(200).json({ statusCode: 200, goesFirst })
+io.on('connect', socket => {
+  console.log(`Someone connected!\nConnection count: ${io.engine.clientsCount}`)
 
-  if (!isFirstPlayer)
-    res.status(200).json({ statusCode: 200, goesFirst: !goesFirst })
+  // handle who goes first
+  if (isFirstPlayer) socket.emit('goes-first', goesFirst)
+  else socket.emit('goes-first', !goesFirst)
 
-  if (isFirstPlayer) isFirstPlayer = false
-})
-
-app.get('/boardstate', (req, res, next) => {
-  res.status(200).json({ statusCode: 200, boardState, turnCount, winner })
-})
-
-app.post('/boardstate', (req, res, next) => {
-  if (!/bs:[_XO]{9}/g.test(req.query.boardstate))
-    return res
-      .status(400)
-      .json({ statusCode: 400, reason: 'Invalid board state' })
-  boardState = req.query.boardstate
-  turnCount += 1 /////////////////
-
-  // Determine if there's a winner, and which symbol
-
-  if (XWinsRegex.test(boardState)) winner = 'X'
-  else if (OWinsRegex.test(boardState)) winner = 'O'
-  else if (turnCount === 9) winner = '_'
-
-  res.status(200).json({
-    statusCode: 200,
-    boardState,
-    turnCount,
-    winner,
-    message: 'Board state set',
+  socket.on('clear', data => {
+    boardState = '_________'
+    turnCount = 0
+    isFirstPlayer = true
+    goesFirst = Math.random() >= 0.5
+    winner = null
+    socket.emit('debug-events', 'Everything cleared!')
+    io.emit('board-state', JSON.stringify({ boardState, turnCount, winner }))
+    console.log('Everything was cleared')
   })
+
+  socket.on('board-state', data => {
+    if (/[_XO]{9}/g.test(data.boardstate)) return
+    boardState = data
+    turnCount += 1
+
+    // test for winner
+    if (XWinsRegex.test(boardState)) winner = 'X'
+    else if (OWinsRegex.test(boardState)) winner = 'O'
+    else if (turnCount === 9) winner = '_'
+
+    io.emit('board-state', JSON.stringify({ boardState, turnCount, winner }))
+    console.log(`Received: ${data}`)
+    console.log(`Emitted to all:`)
+    console.log({ boardState, turnCount, winner })
+  })
+
+  socket.on('debug', data => {
+    console.log(data)
+    socket.emit('debug', 'debuggg')
+  })
+
+  socket.on('disconnect', reason => {
+    console.log(
+      `Someone disconnected!\nConnection count: ${io.engine.clientsCount}\nReason: ${reason}`
+    )
+  }) // Special -> Occurs on disconnect
 })
 
-app.listen(port, () => console.log(`Listening on *:${port}`))
+io.listen(port)
+
+console.log(`Listening on port *:${port}`)
